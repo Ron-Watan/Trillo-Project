3a03d1b5c8d0f9601b9b8e65215db9dc40b8ef72
Ron-Watan <ronwatan@outlook.com>
2023-05-03 03:37:45 -0700

    delet show viewpport

--- script.js
+++
@@ -1,7 +0,0 @@
-const currentViewPort = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-
-function viewPortSize() {
-  location.reload()
-}
-document.getElementById("current--vh").innerHTML = currentViewPort
-
